5971da649cad6fc5413d22acb4eb9924f19fbbec
ShubhamSKA <skumarag@purdue.edu>
2023-03-23 10:19:51 -0400

    cleaned up snake game

--- Snake/Snake_v2.py
+++ Snake/Snake_v2.py
@@ -1,13 +1,12 @@
 from turtle import *
-from time import *
 from msvcrt import *
-from random import *
-from os import *
+from random import randint
 
 def food_gen():
     food.penup()
-    x=(randint((-200/5),(200/5)))*5
-    y=(randint((-120/5),(130/5)))*5
+    food.speed(0)
+    x=(randint((-200/advancement),(200/advancement)))*advancement
+    y=(randint((-120/advancement),(130/advancement)))*advancement
     food.setpos(x,y)
     food.dot(5,"orange")
     return(x,y)
@@ -56,7 +55,7 @@ def advance(length):
         tails.append(tail)
         tails[-1].speed(0)
         tails[-1].pendown()
-        head.forward(5)
+        head.forward(advancement)
         tails[-1].setposition(head.position())
     else:
         tails[0].undo()
@@ -66,11 +65,48 @@ def advance(length):
         tails.append(sub)
         tails[-1].speed(0)
         tails[-1].pendown()
-        head.forward(5)
+        head.forward(advancement)
         tails[-1].setposition(head.position())
         
+def initialization():
+    head.width(5)
 
+    food.ht()
+    head.ht()
+    ht()
+    draw.ht()
+    points.ht()
 
+    head.pencolor("green")
+
+    head.speed(velocity)
+    food.speed(0)
+    draw.speed(0)
+    points.speed(0)
+
+    points.penup()
+    point_update(total)
+    draw.penup()
+    draw.goto(-205,135)
+    draw.pendown()
+    draw.goto(205,135)
+    draw.goto(205,-125)
+    draw.goto(-205,-125)
+    draw.goto(-205,135)
+
+    head.penup()
+    head.goto(-150,0)
+    while head.xcor()<-100:
+        advance(length)
+
+def user_input():
+    if kbhit():
+        if ui!=getch:
+            ui=getch()
+            if ui in [b'w',b'a',b's',b'd'] and ui!=user_direction and user_direction!=invalid[ui]:
+                user_direction=ui
+                defined_direction=list(str(user_direction))[2]
+                direction_change(defined_direction)
 
 tails=[]
 
@@ -81,6 +117,8 @@ invalid={b'w':b's',b'a':b'd',b's':b'w',b'd':b'a'}
 total=0
 length=10
 velocity=11
+advancement=5
+ui=0
 
 setup(450,340)
 head=Turtle()
@@ -88,39 +126,9 @@ food=Turtle()
 draw=Turtle()
 points=Turtle()
 
-head.width(5)
-
-food.ht()
-head.ht()
-ht()
-draw.ht()
-points.ht()
-
-head.pencolor("green")
-
-head.speed(velocity)
-food.speed(0)
-draw.speed(0)
-points.speed(0)
-
-points.penup()
-point_update(total)
-draw.penup()
-draw.goto(-205,135)
-draw.pendown()
-draw.goto(205,135)
-draw.goto(205,-125)
-draw.goto(-205,-125)
-draw.goto(-205,135)
-
-head.penup()
-head.goto(-150,0)
-while head.xcor()<-100:
-    advance(length)
+initialization()
 
 food_coor=food_gen()
-food_x=food_coor[0]
-food_y=food_coor[1]
 
 while game!="start":
     start=input("Should the game start (Y/N)?").lower()
@@ -131,27 +139,21 @@ while game!="end":
     if head.xcor()>=204 or head.xcor()<-204 or head.ycor()>=134 or head.ycor()<=-124:
         game='end'
 
-    if kbhit():
-        ui=getch()
-        if ui in [b'w',b'a',b's',b'd'] and ui!=user_direction and user_direction!=invalid[ui]:
-            user_direction=ui
-            defined_direction=list(str(user_direction))[2]
-            direction_change(defined_direction)   
- 
+    user_input()   
+
     advance(length)
 
-    if int(head.xcor())==food_x and int(head.ycor())==food_y or getcolor(head.xcor(),head.ycor())=='orange':
+    color_under=getcolor(head.xcor(),head.ycor())
+
+    if int(head.position()==food_coor or color_under=='orange'):
         food.clear()
-        length+=1
+        length+=2
         food_coor=food_gen()
-        food_x=food_coor[0]
-        food_y=food_coor[1]
         total+=4
         point_update(total)
-    color_under=getcolor(head.xcor(),head.ycor())
-    if color_under=='green':
+    
+    if color_under=='green' or color_under=='black':
         game='end'
-    if color_under=='black':
-        game='end'
+
 screen=Screen()
 screen.exitonclick()
